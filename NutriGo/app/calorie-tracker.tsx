3d3acb8e91aa892def569258bb66c9379609dbcd
janmlakar <jan.mlakar99@gmail.com>
2024-05-27 09:37:25 +0200

    Food Search

--- NutriGo/app/calorie-tracker.tsx
+++ NutriGo/app/calorie-tracker.tsx
@@ -1,12 +1,19 @@
 import FoodItem, { FoodItemProps } from '@/components/FoodItem';
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 const styles = StyleSheet.create({
   container: {
       flex: 1,
       backgroundColor: '#fff',
       padding: 10,
+      gap: 10,
+  },
+  input: {
+      padding: 10,
+      backgroundColor: 'gainsboro',
+      borderRadius: 20,
   },
 });
 
@@ -17,9 +24,18 @@ const foodItems = [
 ]
 
 export default function CalorieTracker() {
+    const [search, setSearch] = useState('');
+
+    const performSearch = () => {
+      console.log('Searching for:', search);
+      setSearch('');
+    }
+
     return (
     //testni seznam
       <View style={styles.container}>
+        <TextInput value={search} onChangeText={setSearch} placeholder='Search...' style={styles.input}/>
+        {search && <Button title='Search' onPress={performSearch} />}
         <FlatList
           data={foodItems}
           renderItem={({ item }: FoodItemProps) => <FoodItem item={item} />}
