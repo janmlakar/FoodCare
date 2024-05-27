7abf8b7ff2a3582a056b2d708fbc83da2ff4f1af
janmlakar <jan.mlakar99@gmail.com>
2024-05-27 13:18:07 +0200

    Apollo client query test

--- NutriGo/app/_layout.tsx
+++ NutriGo/app/_layout.tsx
@@ -7,6 +7,16 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import ScreenTemplate from './ScreenTemplate';
 import * as NavigationBar from 'expo-navigation-bar';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://melokovka.eu-central-a.ibm.stepzen.net/api/impressive-turkey/__graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization:
+      "apikey melokovka::local.net+1000::01d832eddc639a2eac959379430577a5d718de95f6d12c95f6b67b0114a815d6"
+  }
+});
 
 const Layout = () => {
   const router = useRouter();
@@ -17,46 +27,48 @@ const Layout = () => {
   }, []);
 
   return (
-    <UserProvider>
-      <SafeAreaProvider>
-        <ScreenTemplate>
-          <LinearGradient
-            colors={[
-              'rgba(10,0,30,1)', 
-              'rgba(20,0,40,1)', 
-              'rgba(35,0,55,1)', 
-              'rgba(50,0,70,1)', 
-              'rgba(70,10,90,1)', 
-              'rgba(90,20,110,1)', 
-              'rgba(50,10,70,1)',  // Darker color towards the bottom
-              'rgba(30,5,50,1)'    // Darkest color at the bottom
-            ]}
-            locations={[0, 0.15, 0.35, 0.55, 0.75, 0.9, 0.95, 1]}
-            style={styles.gradientBackground}
-          >
-            <View style={styles.container}>
-              <View style={styles.content}>
-                <Slot />
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <SafeAreaProvider>
+          <ScreenTemplate>
+            <LinearGradient
+              colors={[
+                'rgba(10,0,30,1)',
+                'rgba(20,0,40,1)',
+                'rgba(35,0,55,1)',
+                'rgba(50,0,70,1)',
+                'rgba(70,10,90,1)',
+                'rgba(90,20,110,1)',
+                'rgba(50,10,70,1)',  // Darker color towards the bottom
+                'rgba(30,5,50,1)'    // Darkest color at the bottom
+              ]}
+              locations={[0, 0.15, 0.35, 0.55, 0.75, 0.9, 0.95, 1]}
+              style={styles.gradientBackground}
+            >
+              <View style={styles.container}>
+                <View style={styles.content}>
+                  <Slot />
+                </View>
+                <View style={[styles.navbar, { paddingBottom: insets.bottom }]}>
+                  <TouchableOpacity onPress={() => router.push('/')}>
+                    <Ionicons name="book" size={24} color="white" />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => router.push('/calorie-tracker')}>
+                    <Ionicons name="nutrition" size={24} color="white" />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => router.push('/statistics')}>
+                    <Ionicons name="stats-chart" size={24} color="white" />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => router.push('/profile')}>
+                    <Ionicons name="person" size={24} color="white" />
+                  </TouchableOpacity>
+                </View>
               </View>
-              <View style={[styles.navbar, { paddingBottom: insets.bottom }]}>
-                <TouchableOpacity onPress={() => router.push('/')}>
-                  <Ionicons name="book" size={24} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => router.push('/calorie-tracker')}>
-                  <Ionicons name="nutrition" size={24} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => router.push('/statistics')}>
-                  <Ionicons name="stats-chart" size={24} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => router.push('/profile')}>
-                  <Ionicons name="person" size={24} color="white" />
-                </TouchableOpacity>
-              </View>
-            </View>
-          </LinearGradient>
-        </ScreenTemplate>
-      </SafeAreaProvider>
-    </UserProvider>
+            </LinearGradient>
+          </ScreenTemplate>
+        </SafeAreaProvider>
+      </UserProvider>
+    </ApolloProvider>
   );
 };
 
--- NutriGo/app/calorie-tracker.tsx
+++ NutriGo/app/calorie-tracker.tsx
@@ -1,19 +1,38 @@
 import FoodItem, { FoodItemProps } from '@/components/FoodItem';
 import React from 'react';
-import { View, Text, StyleSheet, FlatList, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TextInput, Button, ActivityIndicator } from 'react-native';
 import { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+query search($ingr: String) {
+  search(ingr: $ingr) {
+    text
+    hints {
+      food {
+        label
+        brand
+        foodId
+        nutrients {
+          ENERC_KCAL
+        }
+      }
+    }
+  }
+}
+`
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      padding: 10,
-      gap: 10,
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+    gap: 10,
   },
   input: {
-      padding: 10,
-      backgroundColor: 'gainsboro',
-      borderRadius: 20,
+    padding: 10,
+    backgroundColor: 'gainsboro',
+    borderRadius: 20,
   },
 });
 
@@ -24,25 +43,41 @@ const foodItems = [
 ]
 
 export default function CalorieTracker() {
-    const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
 
-    const performSearch = () => {
-      console.log('Searching for:', search);
-      setSearch('');
-    }
+  const {data, loading, error} = useQuery(query, {
+    variables: { ingr: "Pizza" },
+  });
 
-    return (
+  const performSearch = () => {
+    console.log('Searching for: pizza');
+    setSearch('');
+  }
+
+  //Apollo client statements
+
+  if (loading) {
+    return <ActivityIndicator />
+  }
+
+  if (error) {
+    return <Text>Error: {error.message}</Text>
+  }
+
+  console.log(JSON.stringify(data, null, 2));
+
+  return (
     //testni seznam
-      <View style={styles.container}>
-        <TextInput value={search} onChangeText={setSearch} placeholder='Search...' style={styles.input}/>
-        {search && <Button title='Search' onPress={performSearch} />}
-        <FlatList
-          data={foodItems}
-          renderItem={({ item }: FoodItemProps) => <FoodItem item={item} />}
-          contentContainerStyle={{ gap: 5}}
-          />
-      </View>
-    );
+    <View style={styles.container}>
+      <TextInput value={search} onChangeText={setSearch} placeholder='Search...' style={styles.input} />
+      {search && <Button title='Search' onPress={performSearch} />}
+      <FlatList
+        data={foodItems}
+        renderItem={({ item }: FoodItemProps) => <FoodItem item={item} />}
+        contentContainerStyle={{ gap: 5 }}
+      />
+    </View>
+  );
 
 
 
