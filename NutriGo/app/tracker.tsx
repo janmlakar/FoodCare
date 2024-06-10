07488e5858aecbe328be1d548fbe72999e1a7ea2
janmlakar <jan.mlakar99@gmail.com>
2024-06-10 17:32:03 +0200

    added remaining calories

--- NutriGo/app/tracker.tsx
+++ NutriGo/app/tracker.tsx
@@ -23,21 +23,25 @@ export default function Tracker() {
     const { user } = useUser();
     const { foodItems, addFoodItem } = useFood(); // Destructure addFoodItem here
 
-    const dailyCalorieIntake = user ? calculateCalorieIntake(
+    const dailyCalorieIntake = user && calculateCalorieIntake(
         user.height,
         user.weight,
         user.age,
         user.gender || 'other',  // Default to 'other' if gender is undefined
         user.activityLevel,
         user.goal
-    ) : 'Not set';
+    );
+
+    const totalCaloriesConsumed = foodItems.reduce((sum, item) => sum + item.nutrients.ENERC_KCAL, 0);
+    const remainingCalories = typeof dailyCalorieIntake === 'number'? dailyCalorieIntake - totalCaloriesConsumed : 0;
+
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={styles.container}>
                 <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
                     <Text style={{ fontSize: 18, fontWeight: '600' }}>Calories</Text>
-                    <Text style={{ fontSize: 18, fontWeight: '600' }}>{dailyCalorieIntake} kcal - USED = REMAINING</Text>
+                    <Text style={{ fontSize: 18, fontWeight: '600' }}>{dailyCalorieIntake} cal - {totalCaloriesConsumed} cal = {remainingCalories} cal</Text>
                 </View>
                 <Text style={{ fontSize: 18, fontWeight: '600' }}>Today's Logged Food</Text>
                 <Link href="/search" asChild>
